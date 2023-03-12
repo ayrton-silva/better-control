a6032be9fec8937ee7bea23ad20a9979cac93cf7
Ayrton Silva <ayrtonricardo2000@hotmail.com>
2023-03-12 20:08:54 -0300

    Setting up ESLint, Stitches and folder structure

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,10 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  backgroundColor: '#fff',
+  color: '#ff0000',
+})
+
+export default function Home() {
+  return <Button>Teste</Button>
+}
